9777fa831b70a4c3848057368a72de6f2f1b9a4a
Thomas Schleicher <thscleicher@edu.aau.at>
2024-03-21 18:09:40 +0100

    added MainActivity.java and configured the manifest to run this activity as the main activity

---
+++ app/src/main/java/se2/group3/gameoflife/frontend/MainActivity.java
@@ -0,0 +1,13 @@
+package se2.group3.gameoflife.frontend;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
