49e3e53ce7d3f7c02eea1d5368eb2b5353bf2c3c
Johanna Klancnik <johannakl@edu.aau.at>
2024-03-24 18:10:14 +0100

    feat: add constructor to class Client.java *add a constructor to be able to pass a string to the server later

--- app/src/main/java/se2/group3/gameoflife/frontend/Client.java
+++ app/src/main/java/se2/group3/gameoflife/frontend/Client.java
@@ -1,4 +1,15 @@
 package se2.group3.gameoflife.frontend;
 
-public class Client {
+public class Client extends Thread{
+
+    private String servername;
+    private int port;
+    private String response;
+    private String request;
+
+    public Client (String request){
+        this.request = request;
+    }
+
+
 }
