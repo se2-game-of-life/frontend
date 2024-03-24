08294ae1309db1c56635aa2a0019d98e033bc132
Johanna Klancnik <johannakl@edu.aau.at>
2024-03-24 18:16:28 +0100

    feat: *commented code* override method run() in Client.java *method run() opens a socket for the communication with server *PrintWriter writes the request *BufferedReader reads the response *code is written in a block comment, because communication with the sever isn´t possible yet

--- app/src/main/java/se2/group3/gameoflife/frontend/Client.java
+++ app/src/main/java/se2/group3/gameoflife/frontend/Client.java
@@ -1,5 +1,13 @@
 package se2.group3.gameoflife.frontend;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class Client extends Thread{
 
     private String servername;
@@ -9,6 +17,30 @@ public class Client extends Thread{
 
     public Client (String request){
         this.request = request;
+    }
+
+    @Override
+    public void run() {
+/*        try {
+            Socket socket = new Socket(serverName, serverPort);
+            PrintWriter output = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true);
+            BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            output.println(request);
+
+            String message = input.readLine();
+
+            input.close();
+            output.close();
+            socket.close();
+
+        } catch (IOException io) {
+            io.printStackTrace();
+        } catch (UnknownHostException ue){
+            ue.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }*/
     }
 
 
