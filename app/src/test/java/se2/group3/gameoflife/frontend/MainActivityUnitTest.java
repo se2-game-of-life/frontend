aa4cae23df2d9853a819f96d5020f3f1cb50651f
Johanna Klancnik <johannakl@edu.aau.at>
2024-03-27 15:25:00 +0100

    feat: add setUp() and breakDown() methods for unit tests

--- app/src/test/java/se2/group3/gameoflife/frontend/MainActivityUnitTest.java
+++ app/src/test/java/se2/group3/gameoflife/frontend/MainActivityUnitTest.java
@@ -4,8 +4,25 @@ package se2.group3.gameoflife.frontend;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-public class MainActivityUnitTest {
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
+public class MainActivityUnitTest {
+    private MainActivity activity;
+
+
+    @BeforeEach
+    public void setUp() {
+        activity = new MainActivity();
+    }
+
+
+    @AfterEach
+    public void breakDown(){
+        activity = null;
+    }
 
 }
 
