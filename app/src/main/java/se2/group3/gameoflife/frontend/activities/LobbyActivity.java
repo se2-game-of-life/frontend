c4cd977c81393d7b6a65bd88c304a8add48d3d37
Thomas <thschleicher@edu.aau.at>
2024-04-07 18:39:44 +0200

    feat: add networking to LobbyActivity

--- app/src/main/java/se2/group3/gameoflife/frontend/activities/LobbyActivity.java
+++ app/src/main/java/se2/group3/gameoflife/frontend/activities/LobbyActivity.java
@@ -11,14 +11,19 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+
 import io.reactivex.disposables.Disposable;
 import se2.group3.gameoflife.frontend.R;
+import se2.group3.gameoflife.frontend.dto.JoinLobbyRequest;
 import se2.group3.gameoflife.frontend.dto.LobbyDTO;
 import se2.group3.gameoflife.frontend.dto.PlayerDTO;
 import se2.group3.gameoflife.frontend.networking.ResponseHandler;
 import se2.group3.gameoflife.frontend.util.SerializationUtil;
 
 public class LobbyActivity extends AppCompatActivity {
+
+    private final static String TAG = "LobbyActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,32 +36,58 @@ public class LobbyActivity extends AppCompatActivity {
             return insets;
         });
 
-        Button b = findViewById(R.id.createButton);
-        b.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                createLobby();
-            }
-        });
+        Button createLobbyButton = findViewById(R.id.createButton);
+        createLobbyButton.setOnClickListener(v -> createLobby(new PlayerDTO("Test Player Name")));
+
+        Button joinGameButton = findViewById(R.id.joinButton);
+        joinGameButton.setOnClickListener(v -> joinLobby(0L, new PlayerDTO("Test Player 2 Name")));
     }
 
-    private void createLobby() {
-        ResponseHandler lobbyResponseHandler = new ResponseHandler() {
+    private void joinLobby(long lobbyID, PlayerDTO player) {
+        ResponseHandler lobbyJoinResponseHandler = new ResponseHandler() {
             @Override
             public void handleMessage(String msg) {
-                LobbyDTO lobbyDTO = (LobbyDTO) SerializationUtil.toObject(msg, LobbyDTO.class);
-                Log.d("Lobby", "Lobby: " + lobbyDTO);
+                try {
+                    LobbyDTO lobbyDTO = (LobbyDTO) SerializationUtil.toObject(msg, LobbyDTO.class);
+                    Log.d(TAG, "Lobby: " + lobbyDTO.getHost());
+                } catch (JsonProcessingException e) {
+                    Log.e(TAG, "Error processing incoming LobbyDTO!", e.getCause());
+                }
             }
-
             @Override
             public void handleError() {}
         };
 
+        Disposable topicSubscription = MainActivity.getNetworkHandler().subscribe("/topic/lobbies", lobbyJoinResponseHandler);
 
+        try {
+            Disposable sendSubscription = MainActivity.getNetworkHandler().send("/app/lobby/join", new JoinLobbyRequest(lobbyID, player));
+        } catch (JsonProcessingException e) {
+            Log.e(TAG, "Error converting PlayerDTO into json string!", e.getCause());
+        }
+    }
+
+    private void createLobby(PlayerDTO player) {
+        ResponseHandler lobbyResponseHandler = new ResponseHandler() {
+            @Override
+            public void handleMessage(String msg) {
+                try {
+                    LobbyDTO lobbyDTO = (LobbyDTO) SerializationUtil.toObject(msg, LobbyDTO.class);
+                    Log.d(TAG, "Lobby: " + lobbyDTO.getLobbyID());
+                } catch (JsonProcessingException e) {
+                    Log.e(TAG, "Error processing incoming LobbyDTO!", e.getCause());
+                }
+            }
+            @Override
+            public void handleError() {}
+        };
 
         Disposable topicSubscription = MainActivity.getNetworkHandler().subscribe("/topic/lobbies", lobbyResponseHandler);
 
-        Disposable sendSubscription = MainActivity.getNetworkHandler().send("/app/lobby/create", new PlayerDTO("Test"));
-
+        try {
+            Disposable sendSubscription = MainActivity.getNetworkHandler().send("/app/lobby/create", player);
+        } catch (JsonProcessingException e) {
+            Log.e(TAG, "Error converting PlayerDTO into json string!", e.getCause());
+        }
     }
 }
