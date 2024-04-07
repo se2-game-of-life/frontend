a75654942f350ff0a53b414beb3fa74784c1ec2e
Johanna Klancnik <johannakl@edu.aau.at>
2024-04-07 19:06:22 +0200

    ref: remove unused imports *import android.util.Log; *import com.fasterxml.jackson.core.exc.StreamWriteException; *import com.fasterxml.jackson.databind.DatabindException; *import java.io.IOException; *import se2.group3.gameoflife.frontend.dto.PlayerDTO;

--- app/src/main/java/se2/group3/gameoflife/frontend/util/SerializationUtil.java
+++ app/src/main/java/se2/group3/gameoflife/frontend/util/SerializationUtil.java
@@ -1,15 +1,8 @@
 package se2.group3.gameoflife.frontend.util;
 
-import android.util.Log;
-
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.exc.StreamWriteException;
-import com.fasterxml.jackson.databind.DatabindException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import java.io.IOException;
-
-import se2.group3.gameoflife.frontend.dto.PlayerDTO;
 
 public class SerializationUtil {
 
