e47b347d6175aafe9df662d4492e612402de5c94
Johanna Klancnik <johannakl@edu.aau.at>
2024-04-07 18:56:00 +0200

    ref: remove unused import 'android.view.View'

--- app/src/main/java/se2/group3/gameoflife/frontend/activities/LobbyActivity.java
+++ app/src/main/java/se2/group3/gameoflife/frontend/activities/LobbyActivity.java
@@ -2,7 +2,6 @@ package se2.group3.gameoflife.frontend.activities;
 
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
