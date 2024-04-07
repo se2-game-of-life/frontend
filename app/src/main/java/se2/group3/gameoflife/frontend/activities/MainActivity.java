1f44338e4234b9007cca7ef50a21d212501cd2ae
Johanna Klancnik <johannakl@edu.aau.at>
2024-04-07 19:02:36 +0200

    ref: remove unused import 'android.util.Log'

--- app/src/main/java/se2/group3/gameoflife/frontend/activities/MainActivity.java
+++ app/src/main/java/se2/group3/gameoflife/frontend/activities/MainActivity.java
@@ -3,7 +3,6 @@ package se2.group3.gameoflife.frontend.activities;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
