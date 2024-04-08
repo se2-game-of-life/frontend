97a854f6f2c633dd39603c47141b41bd6d6471f7
Johanna Klancnik <johannakl@edu.aau.at>
2024-04-08 17:00:44 +0200

    feat: show playernames in StartGameActivity.java *first invisible *visible if online

--- app/src/main/java/se2/group3/gameoflife/frontend/activities/StartGameActivity.java
+++ app/src/main/java/se2/group3/gameoflife/frontend/activities/StartGameActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -18,11 +19,14 @@ import io.reactivex.disposables.Disposable;
 import se2.group3.gameoflife.frontend.R;
 import se2.group3.gameoflife.frontend.dto.JoinLobbyRequest;
 import se2.group3.gameoflife.frontend.dto.LobbyDTO;
+import se2.group3.gameoflife.frontend.dto.PlayerDTO;
 import se2.group3.gameoflife.frontend.networking.ResponseHandler;
 import se2.group3.gameoflife.frontend.util.SerializationUtil;
 
 public class StartGameActivity extends AppCompatActivity {
     private static final String TAG = "StartGameActivity";
+    private boolean waiting = true;
+    private int numberPlayers = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,9 +39,6 @@ public class StartGameActivity extends AppCompatActivity {
             return insets;
         });
 
-        TextView lobbyID = findViewById(R.id.lobbyID);
-        LobbyDTO lobbyDTO = LobbyActivity.getLobbyDTO();
-        lobbyID.setText("ID: " + lobbyDTO.getLobbyID());
 
 
         findViewById(R.id.buttonReturnToLobby).setOnClickListener(new View.OnClickListener() {
@@ -47,5 +48,48 @@ public class StartGameActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        findViewById(R.id.StartButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                waiting = false;
+                //todo: change to gameplay activity
+            }
+        });
+
+        TextView lobbyID = findViewById(R.id.lobbyID);
+        LobbyDTO lobbyDTO = LobbyActivity.getLobbyDTO();
+        lobbyID.setText("ID: " + lobbyDTO.getLobbyID());
+
+        while(waiting){
+            numberPlayers = lobbyDTO.getPlayers().length;
+            PlayerDTO[] players = lobbyDTO.getPlayers();
+            for (int i = 1; i <= numberPlayers; i++){
+                switch(i){
+                    case 1:
+                        Button player1 = findViewById(R.id.player1Button);
+                        player1.setText(players[0].getPlayerName());
+                        player1.setVisibility(View.VISIBLE);
+                        break;
+                    case 2:
+                        Button player2 = findViewById(R.id.player2Button);
+                        player2.setText(players[1].getPlayerName());
+                        player2.setVisibility(View.VISIBLE);
+                        break;
+                    case 3:
+                        Button player3 = findViewById(R.id.player3Button);
+                        player3.setText(players[2].getPlayerName());
+                        player3.setVisibility(View.VISIBLE);
+                        break;
+                    case 4:
+                        Button player4 = findViewById(R.id.player4Button);
+                        player4.setText(players[3].getPlayerName());
+                        player4.setVisibility(View.VISIBLE);
+                        break;
+                    default:
+                }
+            }
+        }
+
     }
 }
