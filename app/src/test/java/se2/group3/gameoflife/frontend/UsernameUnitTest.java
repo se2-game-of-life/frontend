658f343eccf2bc01be121f036c896a86a78cec55
Johanna Klancnik <johannakl@edu.aau.at>
2024-04-11 17:56:37 +0200

    ref: remove 'public' modifier

--- app/src/test/java/se2/group3/gameoflife/frontend/UsernameUnitTest.java
+++ app/src/test/java/se2/group3/gameoflife/frontend/UsernameUnitTest.java
@@ -9,17 +9,17 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 import se2.group3.gameoflife.frontend.game.Username;
 
-public class UsernameUnitTest {
+class UsernameUnitTest {
 
     @ParameterizedTest
     @ValueSource(strings = {"Johanna01", "Thomas", "Flo007", "Anastasiia3", "Aya"})
-    public void testCorrectUsernames(String name){
+    void testCorrectUsernames(String name){
         assertTrue(Username.checkUsername(name));
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"", "01", "70", "!", ":)", "Ana!", "0Jo"})
-    public void testInCorrectUsernames(String name) {
+    void testInCorrectUsernames(String name) {
         assertFalse(Username.checkUsername(name));
     }
 
