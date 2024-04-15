ef0652464036e164b783b79edd8a52d66856d5dc
Thomas Schleicher <thschleicher@edu.aau.at>
2024-04-15 23:38:26 +0200

    feat: add parcel support for inter activity exchange

--- app/src/main/java/se2/group3/gameoflife/frontend/dto/LobbyDTO.java
+++ app/src/main/java/se2/group3/gameoflife/frontend/dto/LobbyDTO.java
@@ -1,9 +1,14 @@
 package se2.group3.gameoflife.frontend.dto;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import androidx.annotation.NonNull;
+
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public class LobbyDTO {
+public class LobbyDTO implements Parcelable {
 
     private final long lobbyID;
     private final PlayerDTO host;
@@ -16,6 +21,24 @@ public class LobbyDTO {
         this.players = players;
     }
 
+    protected LobbyDTO(Parcel in) {
+        lobbyID = in.readLong();
+        host = in.readParcelable(PlayerDTO.class.getClassLoader());
+        players = in.createTypedArray(PlayerDTO.CREATOR);
+    }
+
+    public static final Parcelable.Creator<LobbyDTO> CREATOR = new Parcelable.Creator<LobbyDTO>() {
+        @Override
+        public LobbyDTO createFromParcel(Parcel in) {
+            return new LobbyDTO(in);
+        }
+
+        @Override
+        public LobbyDTO[] newArray(int size) {
+            return new LobbyDTO[0];
+        }
+    };
+
     public long getLobbyID() {
         return this.lobbyID;
     }
@@ -27,4 +50,17 @@ public class LobbyDTO {
     public PlayerDTO[] getPlayers() {
         return players;
     }
+
+    @Override
+    public void writeToParcel(@NonNull Parcel dest, int flags) {
+        dest.writeLong(lobbyID);
+        dest.writeParcelable(host, flags);
+        dest.writeTypedArray(players, flags);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
 }
