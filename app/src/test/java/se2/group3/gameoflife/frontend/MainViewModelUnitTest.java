10f4000f369b58ed911ddaf1bc375906646a4563
Johanna Klancnik <johannakl@edu.aau.at>
2024-04-15 20:38:31 +0200

    fix: adapt UserUnitTest.java to MainViewModelUnitTest.java

--- app/src/test/java/se2/group3/gameoflife/frontend/MainViewModelUnitTest.java
+++ app/src/test/java/se2/group3/gameoflife/frontend/MainViewModelUnitTest.java
@@ -5,44 +5,35 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import se2.group3.gameoflife.frontend.game.User;
+import se2.group3.gameoflife.frontend.game.MainViewModel;
 
-class UserUnitTest {
-    private User user;
+class MainViewModelUnitTest {
+    private MainViewModel mainViewModel;
     @BeforeEach
     public void setUp(){
-        this.user = new User();
+        this.mainViewModel = new MainViewModel();
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"Johanna01", "Thomas", "Flo007", "Anastasiia3", "Aya"})
     void testCorrectUsernames(String name){
-        user.setUsername(name);
-        assertTrue(user.checkUsername());
+        assertTrue(mainViewModel.checkUsername(name));
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"", "01", "70", "!", ":)", "Ana!", "0Jo"})
     void testInCorrectUsernames(String name) {
-        user.setUsername(name);
-        assertFalse(user.checkUsername());
+        assertFalse(mainViewModel.checkUsername(name));
     }
 
-    @Test
-    void testConstructor(){
-        User user1 = new User("Johanna");
-        Assertions.assertEquals("Johanna", user1.getUsername());
-    }
 
     @AfterEach
     public void breakDown(){
-        this.user = null;
+        this.mainViewModel = null;
     }
 
 
