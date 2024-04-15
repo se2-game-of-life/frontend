f2de14f17b2c9dc06b6544eda66b86bd6ffe6b9e
Thomas Schleicher <thschleicher@edu.aau.at>
2024-04-15 23:40:03 +0200

    refactor: StartGameActivity business logic to StartGameViewModel

--- app/src/main/java/se2/group3/gameoflife/frontend/activities/StartGameActivity.java
+++ app/src/main/java/se2/group3/gameoflife/frontend/activities/StartGameActivity.java
@@ -2,7 +2,6 @@ package se2.group3.gameoflife.frontend.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -12,23 +11,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.lifecycle.ViewModelProvider;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-
-import io.reactivex.disposables.Disposable;
 import se2.group3.gameoflife.frontend.R;
 import se2.group3.gameoflife.frontend.dto.LobbyDTO;
 import se2.group3.gameoflife.frontend.dto.PlayerDTO;
-import se2.group3.gameoflife.frontend.networking.ResponseHandler;
-import se2.group3.gameoflife.frontend.util.SerializationUtil;
+import se2.group3.gameoflife.frontend.viewmodels.StartGameViewModel;
 
 
 public class StartGameActivity extends AppCompatActivity {
-    private static final String TAG = "StartGameActivity";
+
+    private StartGameViewModel startGameViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        startGameViewModel = new ViewModelProvider(this).get(StartGameViewModel.class);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_start_game);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
@@ -36,8 +34,6 @@ public class StartGameActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-
 
         findViewById(R.id.buttonReturnToLobby).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -50,68 +46,46 @@ public class StartGameActivity extends AppCompatActivity {
         findViewById(R.id.StartButton).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                //todo: change to gameplay activity
+                throw new UnsupportedOperationException();
             }
         });
 
-        LobbyDTO lobbyDTO = LobbyActivity.getLobbyDTO();
-        updateLobby(lobbyDTO);
+//        LobbyDTO lobbyInformation = getIntent().getParcelableExtra("lobbyDTO");
+//        if(lobbyInformation == null) throw new RuntimeException("LobbyDTO not found in the StartGameActivity");
+//        updateLobby(lobbyInformation);
+
+//        startGameViewModel.getLobby().observe(StartGameActivity.this, this::updateLobby);
     }
 
-    private void updateLobby(LobbyDTO lobbyDTO) {
-        TextView lobbyID = findViewById(R.id.lobbyID);
-        lobbyID.setText("ID: " + lobbyDTO.getLobbyID());
-        int numberPlayers = lobbyDTO.getPlayers().length;
-        PlayerDTO[] players = lobbyDTO.getPlayers();
-        for (int i = 1; i <= numberPlayers; i++){
-            switch(i){
-                case 1:
-                    Button player1 = findViewById(R.id.player1Button);
-                    player1.setText(players[0].getPlayerName());
-                    player1.setVisibility(View.VISIBLE);
-                    break;
-                case 2:
-                    Button player2 = findViewById(R.id.player2Button);
-                    player2.setText(players[1].getPlayerName());
-                    player2.setVisibility(View.VISIBLE);
-                    break;
-                case 3:
-                    Button player3 = findViewById(R.id.player3Button);
-                    player3.setText(players[2].getPlayerName());
-                    player3.setVisibility(View.VISIBLE);
-                    break;
-                case 4:
-                    Button player4 = findViewById(R.id.player4Button);
-                    player4.setText(players[3].getPlayerName());
-                    player4.setVisibility(View.VISIBLE);
-                    break;
-                default:
-            }
-        }
-    }
-
-    private void subscribeToLobby(long lobbyID) {
-        ResponseHandler lobbyResponseHandler = new ResponseHandler() {
-            @Override
-            public void handleMessage(String msg) {
-                try {
-                    LobbyDTO lobbyDTO = (LobbyDTO) SerializationUtil.toObject(msg, LobbyDTO.class);
-                    updateLobby(lobbyDTO);
-                } catch (JsonProcessingException e) {
-                    Log.e(TAG, "Error processing incoming LobbyDTO!", e.getCause());
-                }
-            }
-
-            /**
-             *Error handling yet missing. Will be implemented in the next sprint.
-             */
-            @Override
-            public void handleError() {}
-        };
-
-        Disposable topicSubscription = MainActivity.getNetworkHandler().subscribe("/topic/lobbies/" + lobbyID, lobbyResponseHandler);
-
-    }
-
+//    private void updateLobby(LobbyDTO lobbyDTO) {
+//        TextView lobbyID = findViewById(R.id.lobbyID);
+//        lobbyID.setText("ID: " + lobbyDTO.getLobbyID());
+//        int numberPlayers = lobbyDTO.getPlayers().length;
+//        PlayerDTO[] players = lobbyDTO.getPlayers();
+//        for (int i = 1; i <= numberPlayers; i++){
+//            switch(i){
+//                case 1:
+//                    Button player1 = findViewById(R.id.player1Button);
+//                    player1.setText(players[0].getPlayerName());
+//                    player1.setVisibility(View.VISIBLE);
+//                    break;
+//                case 2:
+//                    Button player2 = findViewById(R.id.player2Button);
+//                    player2.setText(players[1].getPlayerName());
+//                    player2.setVisibility(View.VISIBLE);
+//                    break;
+//                case 3:
+//                    Button player3 = findViewById(R.id.player3Button);
+//                    player3.setText(players[2].getPlayerName());
+//                    player3.setVisibility(View.VISIBLE);
+//                    break;
+//                case 4:
+//                    Button player4 = findViewById(R.id.player4Button);
+//                    player4.setText(players[3].getPlayerName());
+//                    player4.setVisibility(View.VISIBLE);
+//                    break;
+//                default:
+//            }
+//        }
+//    }
 }
---
+++ app/src/main/java/se2/group3/gameoflife/frontend/viewmodels/StartGameViewModel.java
@@ -0,0 +1,39 @@
+package se2.group3.gameoflife.frontend.viewmodels;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.schedulers.Schedulers;
+import se2.group3.gameoflife.frontend.dto.LobbyDTO;
+import se2.group3.gameoflife.frontend.networking.WebsocketClient;
+
+public class StartGameViewModel extends ViewModel {
+    private final WebsocketClient websocketClient = WebsocketClient.getInstance();
+
+    private final MutableLiveData<LobbyDTO> lobbyDTO = new MutableLiveData<>();
+    private final MutableLiveData<String> errorMessage = new MutableLiveData<>();
+
+    private CompositeDisposable disposables;
+
+    public LiveData<LobbyDTO> getLobby() {
+        return lobbyDTO;
+    }
+
+    public void getLobbyUpdates(long lobbyID) {
+        disposables.add(websocketClient.subscribe("/topic/lobbies/" + lobbyID, LobbyDTO.class)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        lobbyDTO::setValue,
+                        error -> errorMessage.setValue(error.getMessage())
+                )
+        );
+    }
+
+    public void dispose() {
+        disposables.dispose();
+    }
+}
