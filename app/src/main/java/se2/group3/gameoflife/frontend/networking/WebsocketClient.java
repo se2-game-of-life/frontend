acbbad94088304460b90def0565cb3437ec7ab88
Thomas Schleicher <thschleicher@edu.aau.at>
2024-04-15 23:41:53 +0200

    feat: restructure WebsocketClient to work with RxJava so that ViewModels can effectively send and subscribe to topics, whilst managing the state of the connection

--- app/src/main/java/se2/group3/gameoflife/frontend/networking/WebsocketClient.java
+++ app/src/main/java/se2/group3/gameoflife/frontend/networking/WebsocketClient.java
@@ -1,69 +1,109 @@
 package se2.group3.gameoflife.frontend.networking;
 
 
+import static se2.group3.gameoflife.frontend.activities.MainActivity.TAG;
 import static se2.group3.gameoflife.frontend.activities.MainActivity.uuid;
 
 import android.util.Log;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+import io.reactivex.Completable;
+import io.reactivex.Observable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.schedulers.Schedulers;
-import se2.group3.gameoflife.frontend.util.SerializationUtil;
 import ua.naiksoftware.stomp.Stomp;
 import ua.naiksoftware.stomp.StompClient;
 
 public class WebsocketClient {
 
+    private static volatile WebsocketClient INSTANCE = null;
     private final StompClient stompClient;
-    private static final String TAG = "Networking";
+    private final ObjectMapper objectMapper;
 
-    public WebsocketClient(String url) {
+    private WebsocketClient(String url) {
         stompClient = Stomp.over(Stomp.ConnectionProvider.OKHTTP, url);
+        objectMapper = new ObjectMapper();
         Log.d(TAG, "STOMP client created!");
     }
 
-
-
-    public void disconnect() {
-        stompClient.disconnect();
-        Log.d(TAG, "STOMP client disconnected!");
-    }
-
-    public void connect() {
-        stompClient.connect();
-        Log.d(TAG, "STOMP client connected!");
-
-        try {
-            send("/app/setIdentifier", uuid);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
+    public static WebsocketClient getInstance(String url) {
+        if (INSTANCE == null) {
+            synchronized (WebsocketClient.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = new WebsocketClient(url);
+                }
+            }
         }
+        return INSTANCE;
     }
 
-    public Disposable subscribe(String topic, ResponseHandler handler) {
-        return stompClient.topic(topic)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(stompMessage -> {
-                    handler.handleMessage(stompMessage.getPayload());
-                    Log.d(TAG, "Received: " + stompMessage.getPayload());
-                }, throwable -> {
-                    handler.handleError();
-                    Log.e(TAG, "Error on subscribe: ", throwable);
-                });
+    public static WebsocketClient getInstance() {
+        return INSTANCE;
+    }
+
+    public Completable disconnect() {
+        return Completable.create(emitter -> {
+            stompClient.disconnect();
+            emitter.onComplete();
+        });
+    }
+
+    public Completable connect() {
+        return Completable.create(emitter -> {
+            stompClient.connect();
+
+            Disposable dis = send("/app/setIdentifier", uuid)
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(
+                            () -> {
+                                Log.d(TAG, "Send message: " + uuid);
+                            }
+                    );
+            emitter.onComplete();
+        });
+    }
+
+    public <T> Observable<T> subscribe(String topic, Class<T> type) {
+        return Observable.create(emitter -> {
+            Disposable disposable = stompClient.topic(topic)
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(stompMessage -> {
+                        try {
+                            T message = type.cast(toObject(stompMessage.getPayload(), type));
+                            Log.d(TAG, "Received Something!");
+                            emitter.onNext(message);
+                        } catch (ClassCastException | JsonProcessingException e) {
+                            emitter.onError(e);
+                        }
+                    }, emitter::onError);
+            emitter.setCancellable(disposable::dispose);
+        });
     }
  
-    public Disposable send(String destination, Object payload) throws JsonProcessingException {
-        return stompClient.send(destination, SerializationUtil.toJsonString(payload))
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(() -> {
-                    Log.d(TAG, "Message Sent: " + SerializationUtil.toJsonString(payload));
-                }, throwable -> {
-                    Log.e(TAG, "Error sending STOMP message: ", throwable);
-                });
+    public Completable send(String destination, Object payload) {
+        return Completable.create(emitter -> {
+            try {
+                String message = toJsonString(payload);
+                Disposable disposable = stompClient.send(destination, message)
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe(
+                                () -> {
+                                    Log.d(TAG, "Send something!");
+                                    emitter.onComplete();
+                                },
+                                emitter::onError
+                        );
+                emitter.setCancellable(disposable::dispose);
+            } catch (JsonProcessingException e) {
+                emitter.onError(e);
+            }
+        });
     }
 
     private String toJsonString(Object object) throws JsonProcessingException {
