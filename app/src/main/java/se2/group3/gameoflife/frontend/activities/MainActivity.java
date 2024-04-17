74b5da1630456c87a8c31e770ae78c17d504ec2b
Thomas Schleicher <thschleicher@edu.aau.at>
2024-04-17 17:19:06 +0200

    refactor: move business logic from MainActivity to MainViewModel

--- app/src/main/java/se2/group3/gameoflife/frontend/activities/MainActivity.java
+++ app/src/main/java/se2/group3/gameoflife/frontend/activities/MainActivity.java
@@ -2,7 +2,6 @@ package se2.group3.gameoflife.frontend.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -10,15 +9,8 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
-import java.util.UUID;
-
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.disposables.Disposable;
-import io.reactivex.schedulers.Schedulers;
 import se2.group3.gameoflife.frontend.R;
-import se2.group3.gameoflife.frontend.dto.PlayerDTO;
-import se2.group3.gameoflife.frontend.game.MainViewModel;
-import se2.group3.gameoflife.frontend.networking.WebsocketClient;
+import se2.group3.gameoflife.frontend.viewmodels.MainViewModel;
 
 /**
  * This class contains the MainActivity. This is the first screen the player sees after opening the app. This activity is used to welcome the player and the option to choose a user name.
@@ -32,30 +24,13 @@ public class MainActivity extends AppCompatActivity {
     private TextView textUser;
     private static String username = null;
 
-
-    /**
-     * Function of the button is defined.
-     * The player is prompted to choose a user name. If the username is correct, the player is directed to the next screen. If the username is incorrect, the player is asked to choose a new one.
-     */
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mainViewModel = new ViewModelProvider(this).get(MainViewModel.class);
 
-        Disposable dis = networkHandler.connect()
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(
-                        () -> Log.d(TAG, "Connected to server!"),
-                        error -> {
-                            throw new RuntimeException();
-                });
-
-
-
-
-        MainViewModel model = new ViewModelProvider(this).get(MainViewModel.class);
+        mainViewModel.connectToServer();
 
         Button check = findViewById(R.id.buttonCheck);
         check.setOnClickListener(v -> {
@@ -73,19 +48,9 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    /**
-     * Is used to change the activity
-     */
-    public void goToNextActivity(){
-        Intent intent = new Intent(this, LobbyActivity.class);
-        startActivity(intent);
-    }
-
-    public static WebsocketClient getNetworkHandler() {
-        return networkHandler;
-    }
-
-    public static String getUsername() {
-        return username;
+    @Override
+    protected void onDestroy() {
+        mainViewModel.dispose();
+        super.onDestroy();
     }
 }
