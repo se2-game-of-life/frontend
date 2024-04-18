37a95bd5f31007d82529b27b0d1486eb4d763162
Johanna Klancnik <johannakl@edu.aau.at>
2024-04-18 16:09:37 +0200

    feat: add functionality to buttons in ChoosePathActivity.java

--- app/src/main/java/se2/group3/gameoflife/frontend/activities/ChoosePathActivity.java
+++ app/src/main/java/se2/group3/gameoflife/frontend/activities/ChoosePathActivity.java
@@ -1,5 +1,6 @@
 package se2.group3.gameoflife.frontend.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -12,6 +13,7 @@ import androidx.core.view.WindowInsetsCompat;
 import se2.group3.gameoflife.frontend.R;
 
 public class ChoosePathActivity extends AppCompatActivity {
+    private boolean college;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +25,19 @@ public class ChoosePathActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        findViewById(R.id.btnCollege).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
+        findViewById(R.id.btnCollege).setOnClickListener(v -> {
+            college = true;
+            choosePath();
         });
+        findViewById(R.id.btnCareer).setOnClickListener(v -> {
+            college = false;
+            choosePath();
+        });
+    }
+
+    private void choosePath(){
+        //todo: communicate decision to server
+        Intent intent = new Intent(this, BoardActivity.class);
+        startActivity(intent);
     }
 }
