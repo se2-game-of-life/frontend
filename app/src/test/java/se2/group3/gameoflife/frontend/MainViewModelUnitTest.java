e1c2fb86643f5c39cdb511fad24ff9eab5316698
Johanna Klancnik <johannakl@edu.aau.at>
2024-04-21 16:20:29 +0200

    feat: add test for getUUID() from MainViewModel.java

--- app/src/test/java/se2/group3/gameoflife/frontend/MainViewModelUnitTest.java
+++ app/src/test/java/se2/group3/gameoflife/frontend/MainViewModelUnitTest.java
@@ -1,16 +1,24 @@
 package se2.group3.gameoflife.frontend;
 
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+
 
 import se2.group3.gameoflife.frontend.viewmodels.MainViewModel;
 
+@RunWith(MockitoJUnitRunner.class)
 class MainViewModelUnitTest {
     private MainViewModel mainViewModel;
     @BeforeEach
@@ -30,6 +38,13 @@ class MainViewModelUnitTest {
         assertFalse(mainViewModel.checkUsername(name));
     }
 
+    @Test
+    void testGetUUID(){
+        String uuid1 = mainViewModel.getUUID();
+        String uuid2 = mainViewModel.getUUID();
+
+        assertEquals(uuid1, uuid2);
+    }
 
     @AfterEach
     public void breakDown(){
