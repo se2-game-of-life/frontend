c6042b36d7401570f7026babe999f0158f49e387
Johanna Klancnik <johannakl@edu.aau.at>
2024-04-29 20:15:27 +0200

    feat: add testcases for StartGameViewModel.java

---
+++ app/src/test/java/se2/group3/gameoflife/frontend/StartGameViewModelUnitTest.java
@@ -0,0 +1,57 @@
+package se2.group3.gameoflife.frontend;
+
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import se2.group3.gameoflife.frontend.dto.LobbyDTO;
+import se2.group3.gameoflife.frontend.dto.PlayerDTO;
+import se2.group3.gameoflife.frontend.viewmodels.StartGameViewModel;
+
+@RunWith(MockitoJUnitRunner.class)
+class StartGameViewModelUnitTest {
+    private StartGameViewModel startGameViewModel;
+
+    @BeforeEach
+    public void setUp(){
+        this.startGameViewModel = new StartGameViewModel();
+    }
+
+    @Test
+    void testSetLobbyDTO_exception(){
+        LobbyDTO lobbyDTO = null;
+        Exception ex = assertThrows(IllegalArgumentException.class, () -> startGameViewModel.setLobbyDTO(lobbyDTO));
+        assertEquals("LobbyDTO not found in the StartGameActivity", ex.getMessage());
+    }
+
+    @Test
+    void testGetLobbyDTO_exception(){
+        Exception ex = assertThrows(IllegalArgumentException.class, () -> startGameViewModel.getLobbyDTO());
+        assertEquals("LobbyDTO is null", ex.getMessage());
+    }
+
+    @Test
+    void testSetLobbyDTO(){
+        LobbyDTO lobbyDTO = new LobbyDTO(1L, new PlayerDTO("name"), null);
+        assertDoesNotThrow(() -> startGameViewModel.setLobbyDTO(lobbyDTO));
+    }
+
+    @Test
+    void testGetLobbyDTO(){
+        startGameViewModel.setLobbyDTO(new LobbyDTO(1L, new PlayerDTO("name"), null));
+        assertDoesNotThrow(() -> startGameViewModel.getLobbyDTO());
+    }
+
+
+    @AfterEach
+    public void breakDown(){
+        this.startGameViewModel = null;
+    }
+}
