955f5a14c85cb159f0aaea83384c46efb4ce599e
Johanna Klancnik <johannakl@edu.aau.at>
2024-05-03 21:38:05 +0200

    feat: add setUp() and breakDown() methods in GameViewModelUnitTest.java

--- app/src/test/java/se2/group3/gameoflife/frontend/GameViewModelUnitTest.java
+++ app/src/test/java/se2/group3/gameoflife/frontend/GameViewModelUnitTest.java
@@ -1,9 +1,26 @@
 package se2.group3.gameoflife.frontend;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import se2.group3.gameoflife.frontend.viewmodels.GameViewModel;
+
+
 @RunWith(MockitoJUnitRunner.class)
 public class GameViewModelUnitTest {
+
+    private GameViewModel gameViewModel;
+
+    @BeforeEach
+    public void setUp(){
+        this.gameViewModel = new GameViewModel();
+    }
+
+    @AfterEach
+    public void breakDown(){
+        this.gameViewModel = null;
+    }
 
 }
