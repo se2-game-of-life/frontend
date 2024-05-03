8c99e49005064e26c375beccc37fb96896641550
Johanna Klancnik <johannakl@edu.aau.at>
2024-05-03 19:45:46 +0200

    feat: add WebsocketClient and CompositeDisposable to GameViewModel.java

--- app/src/main/java/se2/group3/gameoflife/frontend/viewmodels/GameViewModel.java
+++ app/src/main/java/se2/group3/gameoflife/frontend/viewmodels/GameViewModel.java
@@ -1,6 +1,21 @@
 package se2.group3.gameoflife.frontend.viewmodels;
+
+import androidx.lifecycle.MutableLiveData;
+
+import io.reactivex.disposables.CompositeDisposable;
+
+import se2.group3.gameoflife.frontend.dto.PlayerDTO;
+import se2.group3.gameoflife.frontend.networking.WebsocketClient;
+
 public class GameViewModel {
+    private final WebsocketClient websocketClient = WebsocketClient.getInstance();
+    private final CompositeDisposable disposables = new CompositeDisposable();
+
     public void choosePath(){
         //todo: communicate decision to server
+    }
+
+    public void dispose() {
+        disposables.dispose();
     }
 }
