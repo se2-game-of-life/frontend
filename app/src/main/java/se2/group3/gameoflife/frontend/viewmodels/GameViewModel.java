064c7f713176015e584214b66dcb8437cca2b2e5
Johanna Klancnik <johannakl@edu.aau.at>
2024-05-03 19:58:50 +0200

    feat: add networking code for choosePath() in GameViewModel.java

--- app/src/main/java/se2/group3/gameoflife/frontend/viewmodels/GameViewModel.java
+++ app/src/main/java/se2/group3/gameoflife/frontend/viewmodels/GameViewModel.java
@@ -2,17 +2,29 @@ package se2.group3.gameoflife.frontend.viewmodels;
 
 import androidx.lifecycle.MutableLiveData;
 
+import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
 
+import io.reactivex.schedulers.Schedulers;
+import se2.group3.gameoflife.frontend.dto.LobbyDTO;
 import se2.group3.gameoflife.frontend.dto.PlayerDTO;
 import se2.group3.gameoflife.frontend.networking.WebsocketClient;
 
 public class GameViewModel {
     private final WebsocketClient websocketClient = WebsocketClient.getInstance();
     private final CompositeDisposable disposables = new CompositeDisposable();
+    private final MutableLiveData<String> errorMessage = new MutableLiveData<>();
 
-    public void choosePath(){
-        //todo: communicate decision to server
+    public void choosePath(PlayerDTO playerDTO){
+        MutableLiveData<PlayerDTO> player = new MutableLiveData<>(playerDTO);
+        disposables.add(websocketClient.subscribe("/game/path", PlayerDTO.class)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        player::setValue,
+                        error -> errorMessage.setValue(error.getMessage())
+                )
+        );
     }
 
     public void dispose() {
