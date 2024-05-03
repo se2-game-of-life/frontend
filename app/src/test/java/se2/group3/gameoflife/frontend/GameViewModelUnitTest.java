11271930f76f4da2b60eac4e637ad3cc5c8b723f
Johanna Klancnik <johannakl@edu.aau.at>
2024-05-03 21:36:59 +0200

    feat: add GameViewModelUnitTest.java class

---
+++ app/src/test/java/se2/group3/gameoflife/frontend/GameViewModelUnitTest.java
@@ -0,0 +1,9 @@
+package se2.group3.gameoflife.frontend;
+
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class GameViewModelUnitTest {
+
+}
