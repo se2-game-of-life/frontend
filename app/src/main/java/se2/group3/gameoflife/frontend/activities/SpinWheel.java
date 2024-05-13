2f8850d7669d09e307ac4d243d429054eb130b7d
nzhurat <zuratnasta2606@icloud.com>
2024-05-13 16:26:28 +0200

    add new variables to SpinWheel activity

--- app/src/main/java/se2/group3/gameoflife/frontend/activities/SpinWheel.java
+++ app/src/main/java/se2/group3/gameoflife/frontend/activities/SpinWheel.java
@@ -8,9 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import io.reactivex.disposables.Disposable;
 import se2.group3.gameoflife.frontend.R;
+import se2.group3.gameoflife.frontend.networking.WebsocketClient;
 
 public class SpinWheel extends AppCompatActivity {
+    private WebsocketClient networkHandler;
+    private Disposable disposable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
