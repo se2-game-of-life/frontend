e7e4c3fce87ba20c7cb7e7cb772b2d17c632b4c9
nzhurat <zuratnasta2606@icloud.com>
2024-05-13 13:28:45 +0200

    Initialize WebSocket client

--- app/src/main/java/se2/group3/gameoflife/frontend/activities/CardActivity.java
+++ app/src/main/java/se2/group3/gameoflife/frontend/activities/CardActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -13,14 +14,16 @@ import se2.group3.gameoflife.frontend.R;
 import se2.group3.gameoflife.frontend.networking.WebsocketClient;
 
 public class CardActivity extends AppCompatActivity {
+    private WebsocketClient networkHandler;
     private boolean buttonOneClicked = false;
     private boolean buttonTwoClicked = false;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_card);
+        networkHandler = WebsocketClient.getInstance("ws://10.0.2.2:8080/gameoflife");
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
