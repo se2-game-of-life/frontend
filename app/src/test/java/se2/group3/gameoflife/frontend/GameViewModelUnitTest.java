c469de9f7fcb268e5c25910f9ce764f3fcc0bcf3
Johanna Klancnik <johannakl@edu.aau.at>
2024-05-13 21:25:37 +0200

    ref: delete testcases in GameViewModelUnitTest.java of refactoring

--- app/src/test/java/se2/group3/gameoflife/frontend/GameViewModelUnitTest.java
+++ app/src/test/java/se2/group3/gameoflife/frontend/GameViewModelUnitTest.java
@@ -1,16 +1,14 @@
 package se2.group3.gameoflife.frontend;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import se2.group3.gameoflife.frontend.dto.PlayerDTO;
+
 import se2.group3.gameoflife.frontend.viewmodels.GameViewModel;
 
 
@@ -23,30 +21,7 @@ class GameViewModelUnitTest {
     public void setUp(){
         this.gameViewModel = new GameViewModel();
     }
-    @Test
-    void testSetPlayerDTO_exception(){
-        PlayerDTO playerDTO = null;
-        Exception ex = assertThrows(IllegalArgumentException.class, () -> gameViewModel.setPlayerDTO(playerDTO));
-        assertEquals("PlayerDTO is null", ex.getMessage());
-    }
 
-    @Test
-    void testGetPlayerDTO_exception(){
-        Exception ex = assertThrows(IllegalArgumentException.class, () -> gameViewModel.getPlayerDTO());
-        assertEquals("PlayerDTO is null", ex.getMessage());
-    }
-
-    @Test
-    void testSetPlayerDTO(){
-        assertDoesNotThrow(() -> gameViewModel.setPlayerDTO(new PlayerDTO("Player")));
-        assertEquals("Player", gameViewModel.getPlayerDTO().getPlayerName());
-    }
-
-    @Test
-    void testGetPlayerDTO(){
-        gameViewModel.setPlayerDTO(new PlayerDTO("Player"));
-        assertDoesNotThrow(() -> gameViewModel.getPlayerDTO());
-    }
 
     @AfterEach
     public void breakDown(){
