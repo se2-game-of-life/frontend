0acd5e8e10c744fd5ba85f0aa7766e7551ed2e80
Johanna Klancnik <johannakl@edu.aau.at>
2024-05-20 18:39:40 +0200

    feat: display player names in the buttons

--- app/src/main/java/se2/group3/gameoflife/frontend/fragments/StatisticsFragment.java
+++ app/src/main/java/se2/group3/gameoflife/frontend/fragments/StatisticsFragment.java
@@ -3,12 +3,25 @@ package se2.group3.gameoflife.frontend.fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.lifecycle.ViewModelProvider;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.List;
 
 import se2.group3.gameoflife.frontend.R;
+import se2.group3.gameoflife.frontend.activities.GameActivity;
+import se2.group3.gameoflife.frontend.activities.StartGameActivity;
+import se2.group3.gameoflife.frontend.dto.LobbyDTO;
+import se2.group3.gameoflife.frontend.dto.PlayerDTO;
+import se2.group3.gameoflife.frontend.fragments.statistics.statisticsPlayerFragment;
+import se2.group3.gameoflife.frontend.viewmodels.GameViewModel;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,6 +29,7 @@ import se2.group3.gameoflife.frontend.R;
  * create an instance of this fragment.
  */
 public class StatisticsFragment extends Fragment {
+    private GameViewModel gameViewModel;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -60,7 +74,38 @@ public class StatisticsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_statistics, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_statistics, container, false);
+        gameViewModel = new ViewModelProvider(this).get(GameViewModel.class);
+        LobbyDTO lobbyDTO = gameViewModel.getLobbyDTO();
+        List<PlayerDTO> players = lobbyDTO.getPlayers();
+        for (int i = 1; i <= players.size(); i++) {
+            switch (i) {
+                case 1:
+                    Button player1 = rootView.findViewById(R.id.button_player1);
+                    player1.setVisibility(View.VISIBLE);
+                    player1.setText(players.get(i).getPlayerName());
+                    break;
+                case 2:
+                    Button player2 = rootView.findViewById(R.id.button_player2);
+                    player2.setVisibility(View.VISIBLE);
+                    player2.setText(players.get(i).getPlayerName());
+                    break;
+                case 3:
+                    Button player3 = rootView.findViewById(R.id.button_player3);
+                    player3.setVisibility(View.VISIBLE);
+                    player3.setText(players.get(i).getPlayerName());
+                    break;
+                case 4:
+                    Button player4 = rootView.findViewById(R.id.button_player4);
+                    player4.setVisibility(View.VISIBLE);
+                    player4.setText(players.get(i).getPlayerName());
+                    break;
+                default:
+                    Log.d("Networking", "default case in statistic fragment");
+            }
+
+        }
+        return rootView;
     }
+
 }
