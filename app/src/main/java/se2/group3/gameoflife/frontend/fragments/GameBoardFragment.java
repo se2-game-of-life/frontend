5c32549551bb6a0f1d5e9f86b6acafc2bdf5c255
nzhurat <zuratnasta2606@icloud.com>
2024-05-24 16:24:50 +0200

    set onclick

--- app/src/main/java/se2/group3/gameoflife/frontend/fragments/GameBoardFragment.java
+++ app/src/main/java/se2/group3/gameoflife/frontend/fragments/GameBoardFragment.java
@@ -1,35 +1,46 @@
 package se2.group3.gameoflife.frontend.fragments;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import se2.group3.gameoflife.frontend.R;
-
+import se2.group3.gameoflife.frontend.viewmodels.GameBoardViewModel;
 
 public class GameBoardFragment extends Fragment {
+
+    private GameBoardViewModel gameBoardViewModel;
 
     public GameBoardFragment() {
         // Required empty public constructor
     }
 
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public static GameBoardFragment newInstance() {
+        return new GameBoardFragment();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_game_board, container, false);
+        gameBoardViewModel = new ViewModelProvider(this).get(GameBoardViewModel.class);
 
-        return inflater.inflate(R.layout.fragment_game_board, container, false);
+        Button btnSpin = rootView.findViewById(R.id.buttonSpin);
+        btnSpin.setOnClickListener(v -> gameBoardViewModel.spinWheel());
 
+        // Add more buttons and their click listeners as needed
+
+        return rootView;
     }
 
-
+    @Override
+    public void onDestroy() {
+        gameBoardViewModel.dispose();
+        super.onDestroy();
+    }
 }
