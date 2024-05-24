5a7ff10f3d69e9706e3c9462d92deeba88eb8e5c
nzhurat <zuratnasta2606@icloud.com>
2024-05-24 22:13:53 +0200

    added LobbyDTO support to GameBoardViewModel

--- app/src/main/java/se2/group3/gameoflife/frontend/viewmodels/GameBoardViewModel.java
+++ app/src/main/java/se2/group3/gameoflife/frontend/viewmodels/GameBoardViewModel.java
@@ -7,25 +7,33 @@ import androidx.lifecycle.ViewModel;
 
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
-
 import io.reactivex.schedulers.Schedulers;
+import se2.group3.gameoflife.frontend.dto.LobbyDTO;
 import se2.group3.gameoflife.frontend.networking.WebsocketClient;
 
 public class GameBoardViewModel extends ViewModel {
+    private static final String TAG = "GameBoardViewModel";
+
     private final WebsocketClient websocketClient = WebsocketClient.getInstance();
     private final CompositeDisposable disposables = new CompositeDisposable();
     private final MutableLiveData<String> errorMessage = new MutableLiveData<>();
 
-    // Add any additional LiveData objects you might need
-    private MutableLiveData<Integer> spinResult = new MutableLiveData<>();
+    // LiveData to hold the entire game state
+    private final MutableLiveData<LobbyDTO> lobbyState = new MutableLiveData<>();
+    // LiveData to hold the spin result extracted from the lobby state
+    private final MutableLiveData<Integer> spinResult = new MutableLiveData<>();
 
     // Method to spin the wheel
     public void spinWheel() {
-        disposables.add(websocketClient.subscribe("/topic/game/", Integer.class)
+        disposables.add(websocketClient.subscribe("/topic/game/", LobbyDTO.class)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(
-                        spinResult::setValue,
+                        lobbyDTO -> {
+                            lobbyState.setValue(lobbyDTO);
+                            // Extract the spin result from the lobby state
+                            spinResult.setValue(lobbyDTO.getSpunNumber());
+                        },
                         error -> errorMessage.setValue(error.getMessage())
                 )
         );
@@ -33,18 +41,32 @@ public class GameBoardViewModel extends ViewModel {
         disposables.add(websocketClient.send("/app/game/spin", "")
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(() -> {
-                }, error -> errorMessage.setValue(error.getMessage()))
+                .subscribe(
+                        () -> {},
+                        error -> errorMessage.setValue(error.getMessage())
+                )
         );
     }
 
-    // Getter method for spin result
+    // Getter method for the entire lobby state
+    public MutableLiveData<LobbyDTO> getLobbyState() {
+        return lobbyState;
+    }
+
+    // Getter method for the spin result
     public MutableLiveData<Integer> getSpinResult() {
         return spinResult;
     }
 
+    // Getter method for error message
+    public MutableLiveData<String> getErrorMessage() {
+        return errorMessage;
+    }
+
     // Clean up disposables
-    public void dispose() {
+    @Override
+    protected void onCleared() {
+        super.onCleared();
         disposables.dispose();
     }
 }
