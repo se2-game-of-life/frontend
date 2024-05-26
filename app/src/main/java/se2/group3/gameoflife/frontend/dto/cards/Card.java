0760cdb9e58c531d951aef58e16f52ae51ef7fc2
Thomas Schleicher <thschleicher@edu.aau.at>
2024-05-26 13:09:32 +0200

    feat: remove parcelable code from Cards

--- app/src/main/java/se2/group3/gameoflife/frontend/dto/cards/Card.java
+++ app/src/main/java/se2/group3/gameoflife/frontend/dto/cards/Card.java
@@ -12,34 +12,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 
 @JsonIgnoreProperties("stability")
-public class Card implements Parcelable {
+public class Card {
 
     @JsonCreator
     public Card() {}
-
-    protected Card(Parcel in) {
-
-    }
-
-    public static final Creator<Card> CREATOR = new Creator<Card>() {
-        @Override
-        public Card createFromParcel(Parcel in) {
-            return new Card(in);
-        }
-
-        @Override
-        public Card[] newArray(int size) {
-            return new Card[size];
-        }
-    };
-
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    @Override
-    public void writeToParcel(@NonNull Parcel dest, int flags) {
-
-    }
 }
--- app/src/main/java/se2/group3/gameoflife/frontend/dto/cards/CareerCard.java
+++ app/src/main/java/se2/group3/gameoflife/frontend/dto/cards/CareerCard.java
@@ -1,9 +1,3 @@
 package se2.group3.gameoflife.frontend.dto.cards;
 
-import android.os.Parcel;
-
-public class CareerCard extends Card {
-    protected CareerCard(Parcel in) {
-        super(in);
-    }
-}
+public class CareerCard extends Card {}
