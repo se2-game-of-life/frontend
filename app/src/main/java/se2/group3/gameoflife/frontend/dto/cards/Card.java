aead6793adf7c3947f550c2948cb97e4711b93b2
Thomas Schleicher <thschleicher@edu.aau.at>
2024-05-26 13:35:20 +0200

    feat: remove unused imports

--- app/src/main/java/se2/group3/gameoflife/frontend/dto/cards/Card.java
+++ app/src/main/java/se2/group3/gameoflife/frontend/dto/cards/Card.java
@@ -1,15 +1,7 @@
 package se2.group3.gameoflife.frontend.dto.cards;
 
-import android.os.Parcel;
-import android.os.Parcelable;
-
-import androidx.annotation.NonNull;
-
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-import java.util.List;
 
 @JsonIgnoreProperties("stability")
 public class Card {
