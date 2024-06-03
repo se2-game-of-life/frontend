1b44bdccc053de80c6c121681890f0db77b09d24
Florian <flo.hager12@gmail.com>
2024-06-03 19:48:11 +0200

    fix: THE BOARD IS RENDERING AGAIN

--- app/src/main/java/se2/group3/gameoflife/frontend/fragments/GameBoardFragment.java
+++ app/src/main/java/se2/group3/gameoflife/frontend/fragments/GameBoardFragment.java
@@ -135,29 +135,39 @@ public class GameBoardFragment extends Fragment {
 
                     // Set text and appearance of cells based on cellDTO presence
                     if (cellDTO != null) {
-                        if(Objects.equals(cellDTO.getType(), "actionCell")) {
+                        if(Objects.equals(cellDTO.getType(), "ACTION")) {
                             cell.setBackgroundResource(R.drawable.cell_action_background);
                         }
-                        else if(Objects.equals(cellDTO.getType(), "addpegCell")) {
+                        else if(Objects.equals(cellDTO.getType(), "FAMILY")) {
                             cell.setBackgroundResource(R.drawable.cell_addpeg_background);
                         }
-                        else if(Objects.equals(cellDTO.getType(), "investCell")) {
-                            cell.setBackgroundResource(R.drawable.cell_invest_background);
-                        }
-                        else if(Objects.equals(cellDTO.getType(), "houseCell")) {
+                        else if(Objects.equals(cellDTO.getType(), "HOUSE")) {
                             cell.setBackgroundResource(R.drawable.cell_house_background);
                         }
-                        else if(Objects.equals(cellDTO.getType(), "paydayCell")) {
+                        else if(Objects.equals(cellDTO.getType(), "CASH")) {
                             cell.setBackgroundResource(R.drawable.cell_payday_background);
                         }
-                        else if(Objects.equals(cellDTO.getType(), "careerCell")) {
+                        else if(Objects.equals(cellDTO.getType(), "CAREER")) {
                             cell.setBackgroundResource(R.drawable.cell_career_background);
                         }
-                        else if(cellDTO.getType().endsWith("StopCell")) {
+
+                        else if(Objects.equals(cellDTO.getType(), "GROW_FAMILY")) {
                             cell.setBackgroundResource(R.drawable.cell_stop_background);
                         }
-                        else if(cellDTO.getType().endsWith("start")) {
-                            cell.setBackgroundResource(R.drawable.cell_start_background);
+                        else if(Objects.equals(cellDTO.getType(), "GRADUATE")) {
+                            cell.setBackgroundResource(R.drawable.cell_stop_background);
+                        }
+                        else if(Objects.equals(cellDTO.getType(), "MARRY")) {
+                            cell.setBackgroundResource(R.drawable.cell_stop_background);
+                        }
+                        else if(Objects.equals(cellDTO.getType(), "MID_LIFE")) {
+                            cell.setBackgroundResource(R.drawable.cell_stop_background);
+                        }
+                        else if(Objects.equals(cellDTO.getType(), "RETIRE_EARLY")) {
+                            cell.setBackgroundResource(R.drawable.cell_stop_background);
+                        }
+                        else if(Objects.equals(cellDTO.getType(), "RETIREMENT")) {
+                            cell.setBackgroundResource(R.drawable.cell_stop_background);
                         }
                     }
 
