7b60ed547ec1f63ae44b8f45cd2c89dd65acba43
Florian <flo.hager12@gmail.com>
2024-06-12 23:01:46 +0200

    fix: forgot files for same fix

--- app/src/main/java/se2/group3/gameoflife/frontend/dto/cards/CareerCardDTO.java
+++ app/src/main/java/se2/group3/gameoflife/frontend/dto/cards/CareerCardDTO.java
@@ -4,31 +4,23 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class CareerCardDTO extends CardDTO {
 
-        private String id;
+
         private String name;
         private int salary;
         private int bonus;
         private boolean needsDiploma;
 
-        public CareerCardDTO(@JsonProperty("id") String id,
+        public CareerCardDTO(
                              @JsonProperty("name") String name,
                              @JsonProperty("salary") int salary,
                              @JsonProperty("bonus") int bonus,
                              @JsonProperty("needsDiploma") boolean needsDiploma) {
-            this.id = id;
             this.name = name;
             this.salary = salary;
             this.bonus = bonus;
             this.needsDiploma = needsDiploma;
         }
 
-        public String getId() {
-            return id;
-        }
-
-        public void setId(String id) {
-            this.id = id;
-        }
 
         public String getName() {
             return name;
--- app/src/main/java/se2/group3/gameoflife/frontend/dto/cards/HouseCardDTO.java
+++ app/src/main/java/se2/group3/gameoflife/frontend/dto/cards/HouseCardDTO.java
@@ -4,31 +4,23 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class HouseCardDTO extends CardDTO {
 
-    private String id;
     private String name;
     private int purchasePrice;
     private int redSellPrice;
     private int blackSellPrice;
 
-    public HouseCardDTO(@JsonProperty("id") String id,
+    public HouseCardDTO(
                         @JsonProperty("name") String name,
                         @JsonProperty("purchasePrice") int purchasePrice,
                         @JsonProperty("redSellPrice") int redSellPrice,
                         @JsonProperty("blackSellPrice") int blackSellPrice) {
-        this.id = id;
         this.name = name;
         this.purchasePrice = purchasePrice;
         this.redSellPrice = redSellPrice;
         this.blackSellPrice = blackSellPrice;
     }
 
-    public String getId() {
-        return id;
-    }
 
-    public void setId(String id) {
-        this.id = id;
-    }
 
     public String getName() {
         return name;
