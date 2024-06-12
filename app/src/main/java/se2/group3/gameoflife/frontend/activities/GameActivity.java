29215163ace52cb280640a1c3bfb9de1ed57ae7b
Thomas Schleicher <114426174+thomas-schleicher@users.noreply.github.com>
2024-06-12 17:25:20 +0200

    feat: add vibration feature for cheating and random intervals (#71)
    * feat: add vibration feature for cheating and random intervals
    
    * refactor: remove redundancy by merging two network calls
    
    * fix: callback parameter removed

--- app/src/main/java/se2/group3/gameoflife/frontend/activities/GameActivity.java
+++ app/src/main/java/se2/group3/gameoflife/frontend/activities/GameActivity.java
@@ -2,7 +2,11 @@ package se2.group3.gameoflife.frontend.activities;
 
 import static se2.group3.gameoflife.frontend.activities.MainActivity.TAG;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.util.Log;
 import android.view.View;
 
@@ -16,6 +20,8 @@ import androidx.lifecycle.ViewModelProvider;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.util.Random;
+
 import se2.group3.gameoflife.frontend.R;
 import se2.group3.gameoflife.frontend.dto.LobbyDTO;
 import se2.group3.gameoflife.frontend.fragments.ChoosePathFragment;
@@ -23,7 +29,25 @@ import se2.group3.gameoflife.frontend.viewmodels.GameViewModel;
 
 public class GameActivity extends AppCompatActivity {
 
+    public interface VibrateCallback {
+        void onCallback();
+    }
+
     private GameViewModel gameViewModel;
+
+    private static final int MIN_INTERVAL = 1000;
+    private static final int MAX_INTERVAL = 5000;
+    private final Random random = new Random();
+    private final Handler handler = new Handler();
+
+    private final Runnable vibrateTask = new Runnable() {
+        @Override
+        public void run() {
+            vibrate();
+            int nextInterval = random.nextInt(MAX_INTERVAL - MIN_INTERVAL + 1) + MIN_INTERVAL;
+            handler.postDelayed(this, nextInterval);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,7 +71,9 @@ public class GameActivity extends AppCompatActivity {
             Log.d(TAG, "Error getting lobbyDTO: " + e.getMessage());
         }
 
-        gameViewModel.startGame(); //start the game
+        gameViewModel.startGame(this::vibrate);
+        vibrateTask.run();
+
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.fragmentContainerView, new ChoosePathFragment())
                 .commit();
@@ -63,4 +89,17 @@ public class GameActivity extends AppCompatActivity {
     private void updateLobby(LobbyDTO lobbyDTO){
         gameViewModel.setLobbyDTO(lobbyDTO);
     }
+
+    private void vibrate() {
+        Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        v.vibrate(VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE));
+    }
+
+    @Override
+    protected void onDestroy() {
+        handler.removeCallbacks(vibrateTask);
+        gameViewModel.dispose();
+        super.onDestroy();
+    }
+
 }
--- app/src/main/java/se2/group3/gameoflife/frontend/viewmodels/GameViewModel.java
+++ app/src/main/java/se2/group3/gameoflife/frontend/viewmodels/GameViewModel.java
@@ -13,6 +13,7 @@ import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
 
 import io.reactivex.schedulers.Schedulers;
+import se2.group3.gameoflife.frontend.activities.GameActivity.VibrateCallback;
 import se2.group3.gameoflife.frontend.dto.LobbyDTO;
 import se2.group3.gameoflife.frontend.networking.WebsocketClient;
 
@@ -23,7 +24,7 @@ public class GameViewModel extends ViewModel {
 
     private MutableLiveData<LobbyDTO> lobbyDTO = new MutableLiveData<>();
 
-    public void startGame() {
+    public void startGame(VibrateCallback callback) {
         LobbyDTO lobby = lobbyDTO.getValue();
         if(lobby == null) throw new RuntimeException("LobbyDTO NULL in GameViewModel!");
         disposables.add(websocketClient.subscribe("/topic/lobbies/" + lobby.getLobbyID(), LobbyDTO.class)
@@ -31,6 +32,19 @@ public class GameViewModel extends ViewModel {
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(
                         lobbyDTO::setValue,
+                        error -> errorMessage.setValue(error.getMessage())
+                )
+        );
+
+        //subscribe to vibrate-events for the lobby
+        disposables.add(websocketClient.subscribe("/topic/lobbies/" + lobby.getLobbyID() + "/vibrate", LobbyDTO.class)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        value -> {
+                            lobbyDTO.setValue(value);
+                            callback.onCallback();
+                        },
                         error -> errorMessage.setValue(error.getMessage())
                 )
         );
@@ -143,6 +157,10 @@ public class GameViewModel extends ViewModel {
                         error -> errorMessage.setValue(error.getMessage())
                 )
         );
+    }
+
+    public void dispose() {
+        disposables.dispose();
     }
 
     public void setLobbyDTO(LobbyDTO lobbyDTO){
